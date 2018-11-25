30020d53b2650fdfb7b09e39c683ff7247b95c1d
kevintudiep <kevintudiep@gmail.com>
2018-11-25 15:00:38 -0600

    completed exercise

--- exercise.rb
+++ exercise.rb
@@ -12,7 +12,15 @@ p seats[0][1]
 seats.each_with_index do |row, row_index|
   row.each_with_index do |seat, seat_index|
     if seat == nil
-      puts "Row #{row_index+1} seat #{seat_index+1} is free"
+      puts "Row #{row_index+1} seat #{seat_index+1} is free. Do you want to sit there? (y/n)"
+      user_input = gets.chomp
+      if user_input == "y"
+        puts "Whats your name?"
+        user_name = gets.chomp
+        row[seat_index] = user_name
+      end
     end
   end
 end
+
+p seats
